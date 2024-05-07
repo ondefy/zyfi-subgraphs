eb65e3eb390fea5573e14fb6075844b53f523617
gbirckan <gbirckan@gmail.com>
2024-05-07 12:52:20 -0300

    feat: turned entity immutable; used loadInBlock

--- zyfi-paymasters-txs/src/L2EthToken.ts
+++ zyfi-paymasters-txs/src/L2EthToken.ts
@@ -34,7 +34,7 @@ export function handleTransfer(event: TransferEvent): void {
   if (event.params.from.equals(BOOTLOADER)) {
     // bootloader sending value means refund
     const id = event.transaction.hash
-    const entity = PaymasterTransaction.load(id)
+    const entity = PaymasterTransaction.loadInBlock(id)
     if (entity) {
       if (entity.refundValue) {
         entity.refundValue = (entity.refundValue as BigInt).plus(event.params.value)
@@ -46,67 +46,3 @@ export function handleTransfer(event: TransferEvent): void {
     }
   }
 }
-
-// Auto generated code
-
-// export function handleMint(event: MintEvent): void {
-//   let entity = new Mint(
-//     event.transaction.hash.concatI32(event.logIndex.toI32())
-//   )
-//   entity.account = event.params.account
-//   entity.amount = event.params.amount
-
-//   entity.blockNumber = event.block.number
-//   entity.blockTimestamp = event.block.timestamp
-//   entity.transactionHash = event.transaction.hash
-
-//   entity.save()
-// }
-
-// export function handleTransfer(event: TransferEvent): void {
-//   let entity = new Transfer(
-//     event.transaction.hash.concatI32(event.logIndex.toI32())
-//   )
-//   entity.from = event.params.from
-//   entity.to = event.params.to
-//   entity.value = event.params.value
-
-//   entity.blockNumber = event.block.number
-//   entity.blockTimestamp = event.block.timestamp
-//   entity.transactionHash = event.transaction.hash
-
-//   entity.save()
-// }
-
-// export function handleWithdrawal(event: WithdrawalEvent): void {
-//   let entity = new Withdrawal(
-//     event.transaction.hash.concatI32(event.logIndex.toI32())
-//   )
-//   entity._l2Sender = event.params._l2Sender
-//   entity._l1Receiver = event.params._l1Receiver
-//   entity._amount = event.params._amount
-
-//   entity.blockNumber = event.block.number
-//   entity.blockTimestamp = event.block.timestamp
-//   entity.transactionHash = event.transaction.hash
-
-//   entity.save()
-// }
-
-// export function handleWithdrawalWithMessage(
-//   event: WithdrawalWithMessageEvent
-// ): void {
-//   let entity = new WithdrawalWithMessage(
-//     event.transaction.hash.concatI32(event.logIndex.toI32())
-//   )
-//   entity._l2Sender = event.params._l2Sender
-//   entity._l1Receiver = event.params._l1Receiver
-//   entity._amount = event.params._amount
-//   entity._additionalData = event.params._additionalData
-
-//   entity.blockNumber = event.block.number
-//   entity.blockTimestamp = event.block.timestamp
-//   entity.transactionHash = event.transaction.hash
-
-//   entity.save()
-// }
